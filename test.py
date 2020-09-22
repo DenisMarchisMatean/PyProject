a1ff393d8aaedfc5bf87ab8fbb44b135f1e1929c
Alexandru Fleser <alexandru.fleser@fortech.ro>
2020-09-22 14:11:13 +0300

    First Commit

---
+++ test.py
@@ -0,0 +1,17 @@
+import math
+import os
+import sys
+
+import requests
+
+# printsys.version)
+print(sys.executable)
+
+
+def greet(who_to_greet):
+    greeting = "Hello, {}".format(who_to_greet)
+    return greeting
+
+
+r = requests.get("https://coreyms.com")
+print(r.status_code)
